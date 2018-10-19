fd8bd046afdfc14467a79da15275a6bdfec80edc
KEEEER <chris5454561@gmail.com>
2018-10-19 14:55:21 +0800

    first commit

---
+++ AppFirstTry/app/src/main/java/com/example/user/appexample/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.user.appexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+		final TextView tv = findViewById(R.id.text_view_id);
+		final Button button = findViewById(R.id.button_id);
+         button.setOnClickListener(new View.OnClickListener() {
+             @Override
+			 public void onClick(View v) {
+                 tv.setText("You are bad!");
+             }
+         });
+
+    }
+}
